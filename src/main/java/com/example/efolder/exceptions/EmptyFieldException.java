632bf6c119faf4c15199ee1f4db8a00e472b99bc
danifoj315 <danifoj315@student.polsl.pl>
2022-11-22 23:49:46 +0100

    fixed create complex user when required fields are null

---
+++ src/main/java/com/example/efolder/exceptions/EmptyFieldException.java
@@ -0,0 +1,9 @@
+package com.example.efolder.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class EmptyFieldException extends BusinessException{
+    public EmptyFieldException(String fieldName) {
+        super(HttpStatus.NOT_ACCEPTABLE.value(), "Field "+ fieldName+" cannot be null");
+    }
+}
--- src/main/java/com/example/efolder/model/dto/requests/CreateEmployeeRequest.java
+++ src/main/java/com/example/efolder/model/dto/requests/CreateEmployeeRequest.java
@@ -1,5 +1,6 @@
 package com.example.efolder.model.dto.requests;
 
+import com.example.efolder.exceptions.BusinessException;
 import com.example.efolder.model.Address;
 import com.example.efolder.model.Employment;
 import com.example.efolder.model.User;
@@ -11,6 +12,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
+import org.springframework.http.HttpStatus;
 
 import javax.validation.constraints.Email;
 import java.util.Date;
@@ -23,12 +25,19 @@ public class CreateEmployeeRequest {
     CreateEmploymentRequest employment;
     CreateAddressRequest address;
 
+    public boolean checkIfAllRequiredAreAvailable() {
+        if (user.checkIfNotEmpty() && employment.checkIfNotNull() && address.checkIfEmpty())
+            return true;
+        throw new BusinessException(HttpStatus.NOT_ACCEPTABLE.value(), "There is something with the required fields");
+    }
+
+
     public User returnBasicUser(){
         return this.user.userRequest();
     }
 
-    public Employment returnEmployment(UserService userService, TeamService teamService){
-        return employment.employmentRequest(userService, teamService);
+    public Employment returnEmployment(UserService userService, TeamService teamService, String username){
+        return employment.employmentRequest(userService, teamService, username);
     }
 
     public Address returnBasicAddress(User user){
@@ -37,7 +46,7 @@ public class CreateEmployeeRequest {
 
     public User employeeRequest(UserService userService, TeamService teamService){
         User user = this.user.userRequest();
-        Employment employment = this.employment.employmentRequest(userService, teamService);
+        Employment employment = this.employment.employmentRequest(userService, teamService, user.getUsername());
         Address address = this.address.addressRequest(user);
         user.setEmployment(employment);
         user.setAddress(address);

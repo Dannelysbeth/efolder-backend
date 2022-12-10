52d49dcd249f298a26ac6fa290b1a09ee502cece
danifoj315 <danifoj315@student.polsl.pl>
2022-12-10 19:52:38 +0100

    change delete user request from DELETE to POST

--- src/main/java/com/example/efolder/api/UserController.java
+++ src/main/java/com/example/efolder/api/UserController.java
@@ -27,27 +27,30 @@ import static com.example.efolder.security.SecurityConfig.BASE_URL;
 @RequestMapping("/api/user")
 public class UserController {
     private final UserService userService;
-    @Secured("ROLE_SUPER_ADMIN")
+
+    @Secured({"ROLE_SUPER_ADMIN", "ROLE_HR_ADMIN"})
     @GetMapping("/all")
-    public ResponseEntity<List<UserRolesResponse>>getAllUsers(){
+    public ResponseEntity<List<UserRolesResponse>> getAllUsers() {
         return ResponseEntity.ok().body(userService.getAllUsers().stream().map(
                 user -> UserRolesResponse.builder()
                         .user(user)
                         .build()
         ).collect(Collectors.toList()));
     }
-    @Secured("ROLE_SUPER_ADMIN")
+
+    @Secured({"ROLE_SUPER_ADMIN", "ROLE_HR_ADMIN"})
     @GetMapping("/employee/all")
-    public ResponseEntity<List<UserResponse>>getAllEmployees(){
+    public ResponseEntity<List<UserResponse>> getAllEmployees() {
         return ResponseEntity.ok().body(userService.getAllUsersThatHaveRole("ROLE_REGULAR_EMPLOYEE").stream().map(
                 user -> UserResponse.builder()
                         .user(user)
                         .build()
         ).collect(Collectors.toList()));
     }
+
     @Secured({"ROLE_SUPER_ADMIN", "ROLE_HR_ADMIN"})
     @GetMapping("/all/{roleName}")
-    public ResponseEntity<List<UserResponse>>getAllUsersWithRole(@PathVariable String roleName){
+    public ResponseEntity<List<UserResponse>> getAllUsersWithRole(@PathVariable String roleName) {
         return ResponseEntity.ok().body(userService.getAllUsersThatHaveRole(roleName).stream().map(
                 user -> UserResponse.builder()
                         .user(user)
@@ -58,32 +61,32 @@ public class UserController {
     @Secured({"ROLE_SUPER_ADMIN", "ROLE_MANAGER", "ROLE_HR_ADMIN", "ROLE_REGULAR_EMPLOYEE"})
 //    @PreAuthorize("hasAnyRole('ROLE_MANAGER','ROLE_SUPER_ADMIN', 'ROLE_HR_ADMIN','ROLE_REGULAR_EMPLOYEE')")
     @GetMapping("/info")
-    public ResponseEntity<LoggedUserInfoResponse>getLoggedUserInfo(){
+    public ResponseEntity<LoggedUserInfoResponse> getLoggedUserInfo() {
         User loggedUser = userService.getLoggedUser();
         String picUrl = null;
-        if(loggedUser.getProfilePicture()!=null)
-            picUrl = BASE_URL+"api/profilePicture/view/"+loggedUser.getId();
+        if (loggedUser.getProfilePicture() != null)
+            picUrl = BASE_URL + "api/profilePicture/view/" + loggedUser.getId();
         return ResponseEntity.ok().body(LoggedUserInfoResponse.builder()
-                        .username(loggedUser.getUsername())
-                        .firstName(loggedUser.getFirstname())
-                        .lastName(loggedUser.getLastname())
-                        .email(loggedUser.getEmail())
-                        .middleName(loggedUser.getMiddleName())
-                        .birthdate(loggedUser.getBirthdate())
-                        .gender(loggedUser.getGender())
-                        .imageUrl(picUrl)
-                        .roles(loggedUser.getRoles().stream().map(s ->
-                            String.valueOf(s.getRoleName()).toString()
+                .username(loggedUser.getUsername())
+                .firstName(loggedUser.getFirstname())
+                .lastName(loggedUser.getLastname())
+                .email(loggedUser.getEmail())
+                .middleName(loggedUser.getMiddleName())
+                .birthdate(loggedUser.getBirthdate())
+                .gender(loggedUser.getGender())
+                .imageUrl(picUrl)
+                .roles(loggedUser.getRoles().stream().map(s ->
+                        String.valueOf(s.getRoleName()).toString()
                 ).collect(Collectors.toList()))
-                        .build()
+                .build()
         );
     }
 
-    @Secured({"ROLE_SUPER_ADMIN"})
+    @Secured({"ROLE_SUPER_ADMIN", "ROLE_HR_ADMIN"})
     @PostMapping("/superAdmin")
-    public ResponseEntity<UserRolesResponse>saveUser(@RequestBody CreateUserRequest userRequest){
+    public ResponseEntity<UserRolesResponse> saveUser(@RequestBody CreateUserRequest userRequest) {
         User user = userRequest.userRequest();
-        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/superAdmin('"+user.getUsername()+"')").toUriString());
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/superAdmin('" + user.getUsername() + "')").toUriString());
         return ResponseEntity.created(uri).body(UserRolesResponse.builder()
                 .user(userService.createSuperAdmin(user))
                 .build());
@@ -91,9 +94,9 @@ public class UserController {
 
     @Secured({"ROLE_SUPER_ADMIN", "ROLE_MANAGER", "ROLE_HR_ADMIN"})
     @PostMapping()
-    public ResponseEntity<UserResponse>create(@RequestBody CreateUserRequest userRequest){
+    public ResponseEntity<UserResponse> create(@RequestBody CreateUserRequest userRequest) {
         User user = userRequest.userRequest();
-        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user('"+user.getUsername()+"')").toUriString());
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user('" + user.getUsername() + "')").toUriString());
         return ResponseEntity.created(uri).body(UserResponse.builder()
                 .user(userService.createRegularEmployee(user))
                 .build());
@@ -101,14 +104,14 @@ public class UserController {
 
     @Secured({"ROLE_SUPER_ADMIN", "ROLE_MANAGER", "ROLE_HR_ADMIN"})
     @PostMapping("/addRole")
-    public ResponseEntity<?>addRoleToUser(@RequestBody RoleToUserRequest roleToUserRequest){
+    public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserRequest roleToUserRequest) {
         userService.addRoleToUser(roleToUserRequest.getUsername(), roleToUserRequest.getRoleName());
         return ResponseEntity.ok().build();
     }
 
     @Secured({"ROLE_SUPER_ADMIN", "ROLE_MANAGER", "ROLE_HR_ADMIN", "ROLE_REGULAR_EMPLOYEE"})
     @PostMapping("/changePassword")
-    public ResponseEntity<UserRolesResponse>changePassword(@RequestBody ChangePasswordRequest passwordRequest){
+    public ResponseEntity<UserRolesResponse> changePassword(@RequestBody ChangePasswordRequest passwordRequest) {
         User loggedUser = userService.getLoggedUser();
         loggedUser = passwordRequest.changePasswordRequest(loggedUser);             //TODO fix here this password
         return ResponseEntity.ok().body(UserRolesResponse.builder()
@@ -118,7 +121,7 @@ public class UserController {
 
     @Secured({"ROLE_SUPER_ADMIN", "ROLE_HR_ADMIN", "ROLE_REGULAR_EMPLOYEE"})
     @PostMapping("/changePassword/{username}")
-    public ResponseEntity<UserRolesResponse>changeUsersPassword(@RequestBody ChangePasswordRequest passwordRequest, @PathVariable String username){
+    public ResponseEntity<UserRolesResponse> changeUsersPassword(@RequestBody ChangePasswordRequest passwordRequest, @PathVariable String username) {
         User user = userService.getUser(username);
         System.out.println(user.getPassword());
         user = passwordRequest.changePasswordRequest(user);
@@ -129,8 +132,8 @@ public class UserController {
     }
 
     @Secured({"ROLE_SUPER_ADMIN", "ROLE_HR_ADMIN"})
-    @DeleteMapping("/{username}")
-    public ResponseEntity<UserResponse> deleteUser(@PathVariable String username){
+    @PostMapping("/delete/{username}")
+    public ResponseEntity<UserResponse> deleteUser(@PathVariable String username) {
         User user = userService.getUser(username);
         userService.deleteUser(username);
         return ResponseEntity.ok().body(UserResponse.builder()

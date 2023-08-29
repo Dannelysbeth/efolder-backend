c9592d3936e9ba33c4f2148532bd1fe68a544ab9
danifoj315 <danifoj315@student.polsl.pl>
2023-08-29 21:26:05 +0200

    changes to application.properties file

--- src/main/java/com/example/efolder/EfolderApplication.java
+++ src/main/java/com/example/efolder/EfolderApplication.java
@@ -18,41 +18,41 @@ public class EfolderApplication {
         SpringApplication.run(EfolderApplication.class, args);
     }
 
-//    @Bean
-//    CommandLineRunner run(UserService userService, RoleService roleService, TeamService teamService, AddressService addressService, UserInfoService userInfoService){
-//        return args ->{
-//            roleService.saveRole(new Role(null, "ROLE_REGULAR_EMPLOYEE"));
-//            roleService.saveRole(new Role(null, "ROLE_HR_ADMIN"));
-//            roleService.saveRole(new Role(null, "ROLE_MANAGER"));
-//            roleService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
-//
-////            teamService.saveTeam(new Team(null, "HR", "Human Resources", new ArrayList<>(), null));
-////            teamService.saveTeam(new Team(null, "Java", "Java Development", new ArrayList<>(), null));
-////            teamService.saveTeam(new Team(null, "ZMiTAC", "The living hell", new ArrayList<>(), null));
-//
-//            userService.saveUser(new User(null,"jtravolta", "password", new ArrayList<>()));
-//            userService.saveUser(new User(null, "smiley", "password", new ArrayList<>()));
-//            userService.saveUser(new User(null, "epierre", "password", new ArrayList<>()));
-////            userService.saveUser(new User(null, "smiley", "password", new ArrayList<>(), null, null, null, null));
-////            userService.saveUser(new User(null, "epierre", "password", new ArrayList<>(), null, null, null, null));
-//
-//            userService.addRoleToUser("jtravolta", "ROLE_MANAGER");
-//            userService.addRoleToUser("jtravolta", "ROLE_REGULAR_EMPLOYEE");
-//            userService.addRoleToUser("smiley", "ROLE_REGULAR_EMPLOYEE");
-//            userService.addRoleToUser("epierre", "ROLE_REGULAR_EMPLOYEE");
-//            userService.addRoleToUser("epierre", "ROLE_HR_ADMIN");
-//            userService.addRoleToUser("epierre", "ROLE_SUPER_ADMIN");
-////            UserInfo userInfo = userInfoService.getUser("smclavish");
-////            addressService.saveAddress(new Address(
-////                    userInfo.getId(),
-////                    userInfo,
-////                    "UK",
-////                    "County of London",
-////                    "London",
-////                    "Baker's Street",
-////                    "226B",
-//        };
-//    }
+    @Bean
+    CommandLineRunner run(UserService userService, RoleService roleService, TeamService teamService, AddressService addressService, UserInfoService userInfoService){
+        return args ->{
+            roleService.saveRole(new Role(null, "ROLE_REGULAR_EMPLOYEE"));
+            roleService.saveRole(new Role(null, "ROLE_HR_ADMIN"));
+            roleService.saveRole(new Role(null, "ROLE_MANAGER"));
+            roleService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+//            teamService.saveTeam(new Team(null, "HR", "Human Resources", new ArrayList<>(), null));
+//            teamService.saveTeam(new Team(null, "Java", "Java Development", new ArrayList<>(), null));
+//            teamService.saveTeam(new Team(null, "ZMiTAC", "The living hell", new ArrayList<>(), null));
+
+            userService.saveUser(new User(null,"jtravolta", "password", new ArrayList<>()));
+            userService.saveUser(new User(null, "smiley", "password", new ArrayList<>()));
+            userService.saveUser(new User(null, "epierre", "password", new ArrayList<>()));
+//            userService.saveUser(new User(null, "smiley", "password", new ArrayList<>(), null, null, null, null));
+//            userService.saveUser(new User(null, "epierre", "password", new ArrayList<>(), null, null, null, null));
+
+            userService.addRoleToUser("jtravolta", "ROLE_MANAGER");
+            userService.addRoleToUser("jtravolta", "ROLE_REGULAR_EMPLOYEE");
+            userService.addRoleToUser("smiley", "ROLE_REGULAR_EMPLOYEE");
+            userService.addRoleToUser("epierre", "ROLE_REGULAR_EMPLOYEE");
+            userService.addRoleToUser("epierre", "ROLE_HR_ADMIN");
+            userService.addRoleToUser("epierre", "ROLE_SUPER_ADMIN");
+//            UserInfo userInfo = userInfoService.getUser("smclavish");
+//            addressService.saveAddress(new Address(
+//                    userInfo.getId(),
+//                    userInfo,
+//                    "UK",
+//                    "County of London",
+//                    "London",
+//                    "Baker's Street",
+//                    "226B",
+        };
+    }
 
     @Bean
     PasswordEncoder passwordEncoder(){
--- src/main/java/com/example/efolder/security/SecurityConfig.java
+++ src/main/java/com/example/efolder/security/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -22,7 +21,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 import java.util.Collections;
 
-import static org.springframework.security.config.http.SessionCreationPolicy.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
@@ -57,12 +56,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(Collections.unmodifiableList(Arrays.asList("*")));
 //        configuration.setAllowedOrigins(Collections.unmodifiableList(Arrays.asList("*")));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
         configuration.addAllowedHeader(CorsConfiguration.ALL);
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();

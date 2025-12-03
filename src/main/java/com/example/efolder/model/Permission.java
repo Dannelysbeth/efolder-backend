1b853e64ced8feb6eb62bd545976be24cb0a1121
Dannelysbeth <danielaf@onet.pl>
2025-12-03 23:52:40 +0100

    added permission

---
+++ src/main/java/com/example/efolder/model/Permission.java
@@ -0,0 +1,24 @@
+package com.example.efolder.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "permissions")
+public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "permission", nullable = false, unique = true)
+    private String permission;
+}
